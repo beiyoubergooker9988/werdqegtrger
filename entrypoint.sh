858ecc28f31d5c6d62162a8c882f4627837a61d5
pemnwe <135837651+beiyoubergooker9988@users.noreply.github.com>
2023-06-08 12:52:40 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='55a65650-fb55-413f-ab7b-15acdb8659f9'
-VMESS_WSPATH='/55a65650-fb55-413f-ab7b-15acdb8659f9-vmess'
-VLESS_WSPATH='/55a65650-fb55-413f-ab7b-15acdb8659f9-vless'
-TROJAN_WSPATH='/55a65650-fb55-413f-ab7b-15acdb8659f9-trojan'
-SS_WSPATH='/55a65650-fb55-413f-ab7b-15acdb8659f9-ss'
+UUID='84dd254f-c437-459f-9d9e-ffb3675b7df8'
+VMESS_WSPATH='/84dd254f-c437-459f-9d9e-ffb3675b7df8-vmess'
+VLESS_WSPATH='/84dd254f-c437-459f-9d9e-ffb3675b7df8-vless'
+TROJAN_WSPATH='/84dd254f-c437-459f-9d9e-ffb3675b7df8-trojan'
+SS_WSPATH='/84dd254f-c437-459f-9d9e-ffb3675b7df8-ss'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
